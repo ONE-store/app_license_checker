b27c9c61c2ef5425ff08fd34e0cc69b0c58c3e84
최종민 <jongmiss@onestorecorp.com>
2019-10-30 13:40:34 +0900

    HandleError guide improvements

--- sample/src/main/java/com/onestore/licensing/sample/MainActivity.java
+++ sample/src/main/java/com/onestore/licensing/sample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.onestore.licensing.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,7 +11,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.onestore.app.licensing.AppLicenseChecker;
-import com.onestore.app.licensing.Enumeration;
 import com.onestore.app.licensing.LicenseCheckerListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private AppLicenseChecker appLicenseChecker;
     private static final String BASE64_PUBLIC_KEY = "INSERT YOUR PUBLIC_KEY";
     private static final String PID = "INSERT YOUR PID";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button flexible = (Button)findViewById(R.id.check_licensing_flexible);
         Button strict = (Button)findViewById(R.id.check_licensing_strict);
+
         flexible.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,27 +47,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void deniedDialog() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage("구매내역이 존재하지 않습니다.\n원스토어에서 구매하시겠습니까?");
-        alertDialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-                Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
-                        "https://onestore.co.kr/userpoc/game/view?pid="+PID));
-                startActivity(marketIntent);
-            }
-        });
-
-        alertDialog.setNegativeButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                // 취소
-            }
-        });
-
-        alertDialog.setCancelable(false);
-        alertDialog.show();
-    }
-
     private class AppLicenseListener implements LicenseCheckerListener {
         @Override
         public void granted(String license, String signature) {
@@ -83,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-
             deniedDialog();
         }
 
@@ -97,33 +76,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, error, Toast.LENGTH_LONG).show();
             }
 
-            if (2000 == errorCode && 2100 > errorCode ) {
-                // TODO: 원스토어에 문의해주세요.
-            } else if (2100 == errorCode) {
-                // TODO: 네트워크 상태를 체크해주세요.
-            } else if (2101 == errorCode) {
-                // TODO: 원스토어 로그인을 취소하였습니다.
-            } else if (2102 == errorCode) {
-                // TODO: 원스토어 서비스 설치중입니다.
-            } else if (2103 == errorCode) {
-                // TODO: 원스토어를 설치해주세요.
-            } else if (2104 == errorCode) {
-                // TODO: 백그라운드 서비스에서는 진행할 수 없습니다.
-            } else if (2 == errorCode) {
-                // TODO: 네트워크 상태를 체크해주세요.
-            } else if (3 == errorCode) {
-                // TODO: 라이브러리를 최신버전으로 업데이트 해주세요.
-            } else if (5 == errorCode) {
-                // TODO: 원스토어에 문의해주세요.
-            } else {
-                // TODO: 원스토어에 문의해주세요.
-            }
+            handleError(errorCode);
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 
     @Override
@@ -132,5 +86,137 @@ public class MainActivity extends AppCompatActivity {
             appLicenseChecker.destroy();
 
         super.onDestroy();
+    }
+
+    private void handleError(int errorCode) {
+        if (2000 == errorCode && 2100 > errorCode ) {
+            unknownErrorDialog();
+        } else if (2100 == errorCode) {
+            goSettingForNetwork();
+        } else if (2101 == errorCode) {
+            retryLoginDialog();
+        } else if (2102 == errorCode) {
+        } else if (2103 == errorCode) {
+            retryInstall();
+        } else if (2104 == errorCode) {
+            retryALC();
+        } else if (1 == errorCode) {
+            retryLoginDialog();
+        }  else if (2 == errorCode) {
+            goSettingForNetwork();
+        } else if (3 == errorCode) {
+            // download library link : https://github.com/ONE-store/app_license_checker
+        } else if (5 == errorCode) {
+            unknownErrorDialog();
+        } else {
+            unknownErrorDialog();
+        }
+    }
+
+    private void retryALC() {
+        if(null == appLicenseChecker)
+            appLicenseChecker = new AppLicenseChecker(MainActivity.this, BASE64_PUBLIC_KEY, new AppLicenseListener());
+        appLicenseChecker.queryLicense();
+    }
+
+
+    private void showDialog(String message,
+                            String positiveText,
+                            String negativeText,
+                            DialogInterface.OnClickListener positiveClickListener,
+                            DialogInterface.OnClickListener negativeClickListener) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage(message);
+        alertDialog.setPositiveButton(positiveText, positiveClickListener);
+        alertDialog.setNegativeButton(negativeText, negativeClickListener);
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+    }
+
+
+    private void deniedDialog() {
+        showDialog(getString(R.string.does_not_exist),
+                "ok",
+                "finish",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                                getString(R.string.app_market_detail_url)+PID));
+                        startActivity(marketIntent);
+                    }
+                },
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+    }
+
+    private void goSettingForNetwork() {
+        showDialog(getString(R.string.move_network_setting_screen),
+                "setting",
+                "finish",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent intent = new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS);
+                        startActivityForResult(intent, 0);
+                    }
+                },
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+    }
+
+
+    private void retryLoginDialog() {
+        showDialog(getString(R.string.required_onestore_login),
+                "retry",
+                "finish",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        retryALC();
+                    }
+                },
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+    }
+
+    private void retryInstall() {
+        showDialog(getString(R.string.required_onestore_service_install),
+                "ok",
+                "finish",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.download_onestore_service_url)));
+                        startActivity(intent);
+                    }
+                },
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+    }
+
+    private void unknownErrorDialog() {
+        showDialog(getString(R.string.unknown_error),
+                "retry",
+                "finish",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        retryALC();
+                    }
+                },
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
     }
 }
